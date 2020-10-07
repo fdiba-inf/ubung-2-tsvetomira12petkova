6f79438c2ea4fad267458374eb6e5f4e1b75a9bc
tsvetomira12 <cvetunka2001@abg.bg>
2020-10-07 06:04:18 +0000

    2

--- src/exercise2/Circle.java
+++ src/exercise2/Circle.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Circle {
 
     public static void main(String[] args) {
+      System.out.println("r:");
         Scanner input = new Scanner(System.in);
-        int Pi = input.Math.Pi;
-        int r = input.nextInt();
-
-        System.out.print("Circumference " + 2*Pi*r);
-
-        int Pii = input.Math.Pi;
-        int 
         
 
-        System.out.println("You entered: " + number + ". Its square root is " + squareRoot);
+        double Pi = 3.14;
+        double r = input.nextDouble();
+        double C = 2*Pi*r;
+        double A = 2*Pi*r*r;
+
+        System.out.println("Circumference " + C);
+        System.out.println("Area " + A  );
     }
 
 }
