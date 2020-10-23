ee68759b38fbb10aff53d3418288e335d9219f4e
repl.it user <replituser@example.com>
2020-10-23 10:39:54 +0000

    e/o

--- src/exercise2/EvenOrOddNumber.java
+++ src/exercise2/EvenOrOddNumber.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 
           public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Gerade oder ungerade Zahl?");
+        System.out.println("Geben Sie eine Zahl ein: ");
 
         int Zahl = input.nextInt();
         if(Zahl%2==0){
-          System.out.println("even");
+          System.out.println("Number is even");
         }
         
         else if (Zahl%2==1){
-          System.out.println("odd");
+          System.out.println("Number is odd");
 
         }
         
